9f1784ec4e718b60e2a58c5f80ae8d4c9f2b5318
AdQuinti <quintidani2@gmail.com>
2024-01-21 12:02:15 +0100

    arreglo ejercicio txt color animacion

--- 01-20-Clase/pag191-192/js/event.js
+++ 01-20-Clase/pag191-192/js/event.js
@@ -1,40 +1,41 @@
 image1 = new Image();
-image1.src = heading1.gif;
+image1.src = "image/heading1.jpg";
 image2 = new Image();
-image2.src = heading1.gif;
+image2.src = "image/heading2.jpg";
 
 function mouseOver (e) {
     if (!e)
         var e = window.event;
 
-        var target = getTarget (e);
-    
-    // cambio de imagen cuando el raton se posiciona encima de la imagen de la cabecera
+    var target = getTarget (e);
+            
+            // cambio de imagen cuando el raton se posiciona encima de la imagen de la cabecera
     if (target.id == "heading") {
         target.src = image2.src;
         return;
     } // fin if - HEADING
 
-    // si un elemento está definido asignarle su correspondiente valor de código en color
+// si un elemento está definido asignarle su correspondiente valor de código en color
     if (target.id)
-    target.style.color = target.id;
+        target.style.color = target.id;
 } // fin fnction - MOUSEOVER
 
 function mouseOut (e) {
     if (!e)
         var e = window.event;
 
-        var target = getTarget (e);
-    
-    // imagen original evento oneMouseOut imagen cabecera
-    if (target.id == heading) {
+    var target = getTarget (e);
+            
+// imagen original evento oneMouseOut imagen cabecera
+    if (target.id == "heading") {
         target.src = image1.src;
         return;
     } // fin if - HEADING
 
-    // si el id de un elemento está definido se le asigna el id al innerHTML para que muestre su color
-    if (target.id)
+// si el id de un elemento está definido se le asigna el id al innerHTML para que muestre su color
+    if (target.id){
         target.innerHTML = target.id;
+    }
 } // fin funciton - MOUSEOUT
 
 // return e.srcElement, e.target
@@ -42,8 +43,8 @@ function getTarget (e) {
     if (e.srcElement)
         return e.srcElement;
     else
-    return e.target;
+        return e.target;
 } // fin function - GETTARGET
 
-document.onemouseover = mouseOver;
-document.onemouseout = mouseOut;
+document.onmouseover = mouseOver;
+document.onmouseout = mouseOut;
